e88b26fe284b9d158cf8f621e61b02285cb4797f
agostonromhanyi <168738330+aromhanyi-fk@users.noreply.github.com>
2025-04-22 12:41:20 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,4 +20,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.compose.compiler) apply false
+    id("org.jetbrains.kotlinx.kover") version "0.8.3"
 }
