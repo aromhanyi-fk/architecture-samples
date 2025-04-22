b73edb93e4b8e5a12ff0bf51d2855683b1cd072f
agostonromhanyi <168738330+aromhanyi-fk@users.noreply.github.com>
2025-04-22 13:48:05 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,4 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.compose.compiler) apply false
-    id("org.jetbrains.kotlinx.kover") version "0.8.3"
-}
-
-tasks.named("test") {
-    finalizedBy("koverHtmlReport", "koverXmlReport")
 }
